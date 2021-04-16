9765f107fb67bae8023dd99587c9c77ddfb2cb6c
delashu <53063128+delashu@users.noreply.github.com>
2021-04-16 19:13:27 +0000

    edits made to vaccine data retrieval and new table_merge.py file creation first pass

--- vaccine_data_retreival.py
+++ vaccine_data_retreival.py
@@ -10,8 +10,6 @@ try:
     cur.execute(cmd)
 except:
     pass
-cmd = "CREATE TABLE statevax (FIPS REAL,state text,country CHAR(2),date CHAR(10),lat REAL,long REAL,vaccine_type text,doses_alloc REAL,doses_shipped REAL, doses_admin REAL,stage_one_dose REAL,stage_two_dose REAL, comb_key text)"
-cur.execute(cmd)
 vx_df = pd.read_csv("https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/us_data/hourly/vaccine_data_us.csv")
 vx_df.to_sql("statevax", con, if_exists='replace', index=False)
 #cur.execute("SELECT * FROM statevax LIMIT 3;")
