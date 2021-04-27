7aa6a0b2202f87c1553157a8175c172fe32c596d
delashu <53063128+delashu@users.noreply.github.com>
2021-04-27 19:03:01 +0000

    addition of docstrings and commenting to vaccine_data_retreival.py

--- vaccine_data_retreival.py
+++ vaccine_data_retreival.py
@@ -1,7 +1,13 @@
+"""
+Retrieve most recent state-level vaccine data.
+The data comes from a github repository maintained by  Johns Hopkins Medicine and the Center for Health Security in the Bloomberg School of Public Health.
+The data is downloaded via the link to the raw csv data.
+The script then places this data into a table in 'CovidVax.db'.
+"""
 import datetime
 import sqlite3
 import pandas as pd
-#connect to a database in order to create tables
+#connect to database in order to create tables
 con = sqlite3.connect('CovidVax.db')
 cur = con.cursor()
 #create try/except statement since we do not want to overwrite existing tables
@@ -10,9 +16,8 @@ try:
     cur.execute(cmd)
 except:
     pass
+#retrieve data from raw csv link (JHU) and place into sql db
 vx_df = pd.read_csv("https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/us_data/hourly/vaccine_data_us.csv")
 vx_df.to_sql("statevax", con, if_exists='replace', index=False)
-#cur.execute("SELECT * FROM statevax LIMIT 3;")
-#print(cur.fetchall())
 con.commit()
 con.close()
