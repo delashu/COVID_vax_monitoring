758bedc75a9cb8865277f828ad171db7a725ccef
na191 <77761345+na191@users.noreply.github.com>
2021-04-27 17:29:36 -0400

    Update test_vaccine_monitoring.py

--- tests/test_vaccine_monitoring.py
+++ tests/test_vaccine_monitoring.py
@@ -3,6 +3,7 @@ COVID Vaccine Monitoring Test File
 """
 from plots import simpleplots, vaccine_by_demographics, comparisonplots
 from unittest.mock import patch
+import pytest
 
 def test_simpleplots():
     """test simpleplots."""
@@ -21,4 +22,14 @@ def test_comparisonplots():
     with patch("plots.plt.show") as show_patch:
         comparisonplots("Alaska", "North Carolina", "Income")
         assert show_patch.called
+        
 
+def test_simpleplot_datapoint():
+    """ test that single data point appears on scatterplot """
+    demographic = "Income"
+    plots.simpleplots(demographic)
+    f, ax = plt.subplots()
+    scat = ax.scatter([0.41490221], [-2.17831414])
+    set1 = scat.get_offsets()
+    set2 = [[0.41490221, -2.17831414]]
+    assert (set1 == set2).all() == True
