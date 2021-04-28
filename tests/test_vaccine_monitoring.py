16d84cf276d6f9264ce1091b2ba1da5aa14f86c9
saahithirao <saahithi.rao24@gmail.com>
2021-04-28 18:16:55 +0000

    adding one test

--- tests/test_vaccine_monitoring.py
+++ tests/test_vaccine_monitoring.py
@@ -1,14 +1,24 @@
 """
 COVID Vaccine Monitoring Test File
 """
-from plots import simpleplots, vaccine_by_demographics, comparisonplots
+from vaccine_monitoring import interactiveplot, simpleplots, vaccine_by_demographics, comparisonplots
 from unittest.mock import patch
 import pytest
 
 def test_simpleplots():
-    """test simpleplots."""
-    with patch("plots.plt.savefig") as show_patch:
-        simpleplots("IncomePerCap")
+    """test that single data point appears on scatterplot for simpleplots."""
+    demographic = "Income"
+    plots.simpleplots(demographic)
+    f, ax = plt.subplots()
+    scat = ax.scatter([0.41490221], [-2.17831414])
+    set1 = scat.get_offsets()
+    set2 = [[0.41490221, -2.17831414]]
+    assert (set1 == set2).all() == True
+
+def test_interactiveplot():
+    """test interactiveplot."""
+    with patch("plots.go.show") as show_patch:
+        interactiveplot()
         assert show_patch.called
     
 def test_vaccine_by_demographics():
@@ -19,17 +29,9 @@ def test_vaccine_by_demographics():
 
 def test_comparisonplots():
     """test comparisonplots."""
-    with patch("plots.plt.show") as show_patch:
+    with patch("plots.plt.savefig") as show_patch:
         comparisonplots("Alaska", "North Carolina", "Income")
         assert show_patch.called
         
 
-def test_simpleplot_datapoint():
-    """ test that single data point appears on scatterplot """
-    demographic = "Income"
-    plots.simpleplots(demographic)
-    f, ax = plt.subplots()
-    scat = ax.scatter([0.41490221], [-2.17831414])
-    set1 = scat.get_offsets()
-    set2 = [[0.41490221, -2.17831414]]
-    assert (set1 == set2).all() == True
+
